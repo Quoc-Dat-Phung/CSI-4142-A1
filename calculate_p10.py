1081092390a1e1d0b5a8d3eb00ad79d0fca8644a
Rxmi-sk <Rslimk3@gmail.com>
2025-03-30 15:04:28 -0400

    Changes

--- calculate_p10.py
+++ calculate_p10.py
@@ -1,25 +1,8 @@
 import pandas as pd
 
 
-def calculate_p10():
+def calculate_p10(results_file):
 
-    while True:
-        try:
-            file_Number = input("Pick what file to test P10 on (1 for USE or 2 for BERT): ")
-            
-            if int(file_Number) == 1:
-                results_file = "C:/Users/zakar/OneDrive/Desktop/CSI-4142-A1/res/a2_res_USE.txt"
-                break
-            elif int(file_Number) == 2:
-                results_file = "C:/Users/zakar/OneDrive/Desktop/CSI-4142-A1/res/a2_res_BERT.txt"
-                break
-            else:
-                raise ValueError("Invalid input. Please enter 1 for USE or 2 for BERT.")
-
-        except ValueError as e:
-            print(f"Error: {e}. Please try again.")
-
-    print("Calculating P10 on file:"+results_file)
     columns = ["query_id", "Q0", "doc_id", "rank", "score", "tag"]
     df = pd.read_csv(results_file, sep="\t", names=columns, header=0)
 
@@ -43,11 +26,23 @@ def calculate_p10():
     return p10_scores
 
 if __name__ == "__main__":
-    p10_scores = calculate_p10()
+    
+    p10_scores1 = calculate_p10("res/a2_res_USE.txt")
+    p10_scores2 = calculate_p10("res/a2_res_BERT.txt")
 
-    if p10_scores is not None:  
+    if p10_scores1 is not None:
+        print("Calculating P10 on file: res/a2_res_USE.txt")
+
         # Print results
-        for query, score in p10_scores.items():
+        for query, score in p10_scores1.items():
+            print(f"Query {query}: P@10 = {score:.2f}")
+    else:
+        print("an erroro occured: P@10 score calculation returned None")
+
+    if p10_scores2 is not None:
+        print("Calculating P10 on file: res/a2_res_BERT.txt")
+        # Print results
+        for query, score in p10_scores2.items():
             print(f"Query {query}: P@10 = {score:.2f}")
     else:
         print("an erroro occured: P@10 score calculation returned None")
